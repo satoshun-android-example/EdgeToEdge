44a7d47e6e606dcbc2bf958dc4b4683f5fdee524
SatoShun <shun.sato1@gmail.com>
2020-01-19 10:44:51 +0000

    hoge

--- app/src/main/java/com/github/satoshun/example/main/fullscreenvideo/FullscreenVideoActivity.kt
+++ app/src/main/java/com/github/satoshun/example/main/fullscreenvideo/FullscreenVideoActivity.kt
@@ -1,11 +1,11 @@
 package com.github.satoshun.example.main.fullscreenvideo
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.github.satoshun.example.R
 import com.github.satoshun.example.databinding.FullscreenVideoBinding
 import com.github.satoshun.example.getContentView
-import dev.chrisbanes.insetter.setEdgeToEdgeSystemUiFlags
 
 class FullscreenVideoActivity : AppCompatActivity(R.layout.fullscreen_video) {
   private lateinit var binding: FullscreenVideoBinding
@@ -14,7 +14,14 @@ class FullscreenVideoActivity : AppCompatActivity(R.layout.fullscreen_video) {
     super.onCreate(savedInstanceState)
     binding = FullscreenVideoBinding.bind(getContentView())
 
-    binding.root.setEdgeToEdgeSystemUiFlags(true)
+//    window.decorView.setEdgeToEdgeSystemUiFlags(true)
+    window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+      or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+      or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+      )
+
     binding.coordinator.setOnApplyWindowInsetsListener { v, insets ->
       insets
     }
