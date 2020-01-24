7c6547286bf7edd899efd9f2b4713d36de9a8155
SatoShun <shun.sato1@gmail.com>
2020-01-24 10:52:19 +0000

    add appbar

--- app/src/main/java/com/github/satoshun/example/main/fitssystemwindow/coordinatorlayout/CoordinatorLayoutActivity.kt
+++ app/src/main/java/com/github/satoshun/example/main/fitssystemwindow/coordinatorlayout/CoordinatorLayoutActivity.kt
@@ -3,10 +3,19 @@ package com.github.satoshun.example.main.fitssystemwindow.coordinatorlayout
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.satoshun.example.R
+import com.github.satoshun.example.databinding.CoordinatorLayoutLayoutBinding
+import com.github.satoshun.example.getContentView
+import dev.chrisbanes.insetter.setEdgeToEdgeSystemUiFlags
 
 class CoordinatorLayoutActivity : AppCompatActivity(R.layout.coordinator_layout_layout) {
+  private lateinit var binding: CoordinatorLayoutLayoutBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setSupportActionBar(findViewById(R.id.toolbar))
+    binding = CoordinatorLayoutLayoutBinding.bind(getContentView())
+
+    setSupportActionBar(binding.toolbar)
+
+    binding.root.setEdgeToEdgeSystemUiFlags(true)
   }
 }
