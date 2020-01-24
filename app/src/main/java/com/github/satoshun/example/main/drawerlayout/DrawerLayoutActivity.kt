8d7d84cccb8425cf415f1a0531aeaedc72017bdf
SatoShun <shun.sato1@gmail.com>
2020-01-24 00:45:14 +0000

    not working when after onCreated

--- app/src/main/java/com/github/satoshun/example/main/drawerlayout/DrawerLayoutActivity.kt
+++ app/src/main/java/com/github/satoshun/example/main/drawerlayout/DrawerLayoutActivity.kt
@@ -3,10 +3,13 @@ package com.github.satoshun.example.main.drawerlayout
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.github.satoshun.example.R
 import com.github.satoshun.example.databinding.DrawerLayoutBinding
 import com.github.satoshun.example.getContentView
 import dev.chrisbanes.insetter.setEdgeToEdgeSystemUiFlags
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class DrawerLayoutActivity : AppCompatActivity(R.layout.drawer_layout) {
   private lateinit var binding: DrawerLayoutBinding
@@ -27,5 +30,15 @@ class DrawerLayoutActivity : AppCompatActivity(R.layout.drawer_layout) {
     toggle.syncState()
 
     window.decorView.setEdgeToEdgeSystemUiFlags(true)
+
+//    binding.toolbar.fitsSystemWindows = true
+
+    lifecycleScope.launch {
+      delay(5000)
+//      binding.toolbar.fitsSystemWindows = false
+      binding.toolbar.fitsSystemWindows = true
+      binding.root.invalidate()
+      println("hoge")
+    }
   }
 }
