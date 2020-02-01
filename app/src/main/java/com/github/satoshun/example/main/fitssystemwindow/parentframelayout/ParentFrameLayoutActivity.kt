5d1bbd4fea29cef5038d2fa933fcdf6f1ebdae65
SatoShun <shun.sato1@gmail.com>
2020-02-01 04:29:23 +0000

    consume

--- app/src/main/java/com/github/satoshun/example/main/fitssystemwindow/parentframelayout/ParentFrameLayoutActivity.kt
+++ app/src/main/java/com/github/satoshun/example/main/fitssystemwindow/parentframelayout/ParentFrameLayoutActivity.kt
@@ -1,12 +1,12 @@
 package com.github.satoshun.example.main.fitssystemwindow.parentframelayout
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.updatePadding
 import com.github.satoshun.example.R
 import com.github.satoshun.example.databinding.ParentFrameLayoutBinding
 import com.github.satoshun.example.getContentView
-import dev.chrisbanes.insetter.setEdgeToEdgeSystemUiFlags
 
 class ParentFrameLayoutActivity : AppCompatActivity(R.layout.parent_frame_layout) {
   private lateinit var binding: ParentFrameLayoutBinding
@@ -16,14 +16,38 @@ class ParentFrameLayoutActivity : AppCompatActivity(R.layout.parent_frame_layout
     binding = ParentFrameLayoutBinding.bind(getContentView())
     setSupportActionBar(binding.toolbar)
 
+    val view = binding.root
+
 //    window.statusBarColor = Color.BLUE
 //    window.navigationBarColor = Color.BLUE
 
-    window.decorView.setEdgeToEdgeSystemUiFlags(true)
+//    window.decorView.setEdgeToEdgeSystemUiFlags(true)
 
-    binding.appbar.setOnApplyWindowInsetsListener { v, insets ->
-      binding.appbar.updatePadding(top = insets.systemWindowInsetTop * 2)
+    view.systemUiVisibility =
+      View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
+    view.setOnApplyWindowInsetsListener { v, insets ->
+      v.updatePadding(
+        top = insets.systemWindowInsetTop,
+        bottom = insets.systemWindowInsetBottom
+      )
       insets.consumeSystemWindowInsets()
     }
+
+    val child = binding.appbar
+    child.setOnApplyWindowInsetsListener { v, insets ->
+      v.updatePadding(
+        top = insets.systemWindowInsetTop,
+        bottom = insets.systemWindowInsetBottom
+      )
+      insets
+    }
+
+//    binding.appbar.setOnApplyWindowInsetsListener { v, insets ->
+//      binding.appbar.updatePadding(top = insets.systemWindowInsetTop * 2)
+//      insets.consumeSystemWindowInsets()
+//    }
   }
 }
