23a167260de498b708b7509ffdeb90db2e30ad91
SatoShun <shun.sato1@gmail.com>
2020-02-12 23:57:24 +0000

    full

--- app/src/main/java/com/github/satoshun/example/main/fullscreenvideo/FullscreenVideoActivity.kt
+++ app/src/main/java/com/github/satoshun/example/main/fullscreenvideo/FullscreenVideoActivity.kt
@@ -3,7 +3,6 @@ package com.github.satoshun.example.main.fullscreenvideo
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.github.satoshun.example.R
 import com.github.satoshun.example.databinding.FullscreenVideoBinding
@@ -20,10 +19,10 @@ class FullscreenVideoActivity : AppCompatActivity(R.layout.fullscreen_video) {
 //    window.decorView.setEdgeToEdgeSystemUiFlags(true)
 
 //    binding.root.systemUiVisibility = 0
-    view.systemUiVisibility =
-      View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//    view.systemUiVisibility =
+//      View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+//        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+//        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
 //    view.systemUiVisibility = view.systemUiVisibility or
 //      View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
